d0cd063f98e613be8101437b8fb124697f5755fe
e raja raghudeep <raja.emani@mapmyindia.com>
2017-01-03 12:05:10 +0530

    Modified demo_new.py to deal with multiple same class detections.

--- tools/demo_new.py
+++ tools/demo_new.py
@@ -48,44 +48,110 @@ NETS = {'vgg16': ('VGG16',
 		'gtsdb': ('gtsdb',
 				'3gtsdb_iter_40000.caffemodel'),
 		'mmi': ('mmi',
-				'mmi_retrained_23_12.caffemodel')}
+				'mmi_26_10_iter_60000.caffemodel')}
 
 
+def save_blobs(bbox,img,name,class_name): #here labels should be a list of label
+     left=int(bbox[0])
+     top=int(bbox[1])
+     right=int(bbox[2])
+     bottom=int(bbox[3])
+     crop_img=img[top:bottom, left:right]
+     crop_img=cv2.resize(crop_img, (100, 100))
+     cv2.imwrite('/home/ce/Documents/py-faster-rcnn/blobs/'+name+'_'+class_name+".jpg",crop_img)
+
+def get_overlap(l1,l2):
+    l1,l2=l1.tolist(),l2.tolist()
+    xa1,ya1,xa2,ya2=l1[0],l1[1],l1[2],l1[3]
+    xb1,yb1,xb2,yb2=l2[0],l2[1],l2[2],l2[3]
+    dx =min(xa2, xb2) -max(xa1, xb1)
+    #print xa2, xb2,xa1, xb1
+    dy = min(ya2, yb2) - max(ya1, yb1)
+    #print dx,dy
+    area_a=(xa2-xa1)*(ya2-ya1)
+    area_b=(xb2-xb1)*(yb2-yb1)
+    if (dx>=0) and (dy>=0):
+        area_i= dx*dy
+        area=area_a+area_b-area_i
+        return float(area_i)/area
+    else:
+        return 0
 def vis_detections(im, class_name, dets, image_name,out_file,thresh=0.5):
     """Draw detected bounding boxes."""
+
     inds = np.where(dets[:, -1] >= thresh)[0]
-   # print inds
+
     if len(inds) == 0:
         return
+    out_file=os.path.join('/home/ce/Documents/py-faster-rcnn/',out_file)
     f=open(out_file,'a')
-    #print image_name
+
+
+
+    im1=im
     im = im[:, :, (2, 1, 0)]
+
     fig, ax = plt.subplots(figsize=(12, 12))
+
     ax.imshow(im, aspect='equal')
+    print inds,len(inds)
+    s=[]
+    for j in inds:
+    	score1=dets[j,-1]
+    	s.append(score1)
+
+    h=np.zeros((len(inds),len(inds)))
+    for k in inds:
+        for l in inds:
+            bbox1=dets[k, :4]
+            bbox2=dets[l, :4]
+            area=get_overlap(bbox1,bbox2)
+            h[k,l]=area
+    #print len(inds)
     for i in inds:
         bbox = dets[i, :4]
         score = dets[i, -1]
-        #print bbox, score
-        ax.add_patch(plt.Rectangle((bbox[0], bbox[1]),bbox[2] - bbox[0],bbox[3] - bbox[1], fill=False,edgecolor='red', linewidth=1.5))
-        ax.text(bbox[0], bbox[1] - 2,'{:s} {:.3f}'.format(class_name, score),bbox=dict(facecolor='blue', alpha=0.5),fontsize=14, color='white')
-       # print bbox
-        f.write(str(image_name)+';'+str(bbox)+';'+str(class_name)+';'+str(score)+'\n')
+        h_area=h[i]
+
+        over_lapped=np.where(h_area>0.2)[0]
+        over_lapped = over_lapped.tolist()
+
+        #print over_lapped
+
+
+        s_new = np.array(s)[over_lapped]
+        t=np.max(s_new)
+        z=np.argmax(s_new)
+        max_lapped = over_lapped[z] #index of the max score under lapped regions
+        #print max_lapped
+
+        if len(over_lapped)==1:
+
+            name=image_name.split('.')[0]
+
+            ax.add_patch(plt.Rectangle((bbox[0], bbox[1]),bbox[2] - bbox[0],bbox[3] - bbox[1], fill=False,edgecolor='green', linewidth=1.5))
+            ax.text(bbox[0], bbox[1] - 2,'{:s} {:.3f}'.format(class_name, score),bbox=dict(facecolor='blue', alpha=0.5),fontsize=14, color='white')
+            f.write(str(image_name)+';'+str(bbox)+';'+str(class_name)+';'+str(score)+'\n')
+            save_blobs(bbox,im1,name,class_name)
+        else:
+
+            if score >=t and max_lapped==i:
+
+                name=image_name.split('.')[0]
+                save_blobs(bbox,im1,name,class_name)
+                ax.add_patch(plt.Rectangle((bbox[0], bbox[1]),bbox[2] - bbox[0],bbox[3] - bbox[1], fill=False,edgecolor='green', linewidth=1.5))
+                ax.text(bbox[0], bbox[1] - 2,'{:s} {:.3f}'.format(class_name, score),bbox=dict(facecolor='blue', alpha=0.5),fontsize=14, color='white')
+                f.write(str(image_name)+';'+str(bbox)+';'+str(class_name)+';'+str(score)+'\n')
 
     ax.set_title(('{} detections with ''p({} | box) >= {:.1f}').format(class_name, class_name,thresh),fontsize=14)
     plt.axis('off')
     plt.tight_layout()
     plt.draw()
     name=image_name.split('.')[0]
-    #name = os.path.basename(image_name)[:-4]
-    print name
-    try:
-        os.mkdir('output_detections')
-    except:
-        pass
-    #print name
-    plt.savefig('output_detections/'+name+'_'+class_name+".png")
+    plt.savefig('/home/ce/Documents/py-faster-rcnn/'+'output_detections/'+name+'_'+class_name+'.jpg')
+
+    #save_blobs(bbox,im1,name,class_name)
     plt.close()
-    plt.draw()
     f.close()
 
 def demo(net, image_name,path,out_file):
@@ -108,8 +174,8 @@ def demo(net, image_name,path,out_file):
 
     else:
         return 0
-    
-   # print im
+
+
     # Detect all object classes and regress object bounds
     timer = Timer()
     timer.tic()
@@ -144,7 +210,7 @@ def parse_args():
                         action='store_true')
     parser.add_argument('--net', dest='demo_net', help='Network to use [vgg16]',
                         choices=NETS.keys(), default='vgg16')
-    parser.add_argument('--path',dest='path',help='Path the test images folder',default='/home/ce/Documents/py-faster-rcnn/data/MMI/data/Images2/')
+    parser.add_argument('--path',dest='path',help='Path the test images folder',default='/home/ce/Documents/py-faster-rcnn/data/MMI/data/test-images/')
     parser.add_argument('--file',dest='test_file',help='Path the test images txt file')
     parser.add_argument('--out_file',dest='out_file',help='Path the results')
 
@@ -156,7 +222,11 @@ if __name__ == '__main__':
     cfg.TEST.HAS_RPN = True  # Use RPN for proposals
 
     args = parse_args()
-
+    try:
+        os.mkdir('output_detections')
+    except:
+        pass
+    #print name
     prototxt = os.path.join(cfg.MODELS_DIR,'..', NETS[args.demo_net][0],'faster_rcnn_end2end', 'test.prototxt')
 #    prototxt = os.path.join(cfg.MODELS_DIR, NETS[args.demo_net][0],'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
 
@@ -203,8 +273,9 @@ if __name__ == '__main__':
         #print im_name
         try:
            # print im_name
+            #im_name = im_name[:-4]
             demo(net,im_name,path,args.out_file)
-            
+
         except:
             continue
         print a,'/',total,'Demo for data/demo/{}'.format(im_name)
