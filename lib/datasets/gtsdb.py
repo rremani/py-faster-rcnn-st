6fb71e8a8574c7169691aaa27a65a12572ed11dd
e raja raghudeep <raja.emani@mapmyindia.com>
2017-01-03 10:11:23 +0530

    by aishwarya

--- lib/datasets/gtsdb.py
+++ lib/datasets/gtsdb.py
@@ -24,21 +24,22 @@ class gtsdb(imdb):
         self._image_set = image_set
         self._devkit_path = devkit_path
         self._data_path = os.path.join(self._devkit_path, 'data')
-        self._classes = ('__background__', # always index 0
-                         'Speed Limit 30',
-                         'Speed Limit 50',
-                         'Speed Limit 60',
-                         'Speed Limit 70',
-                         'Speed Limit 80',
-                         'Speed Limit 100',
-                         'Speed Limit 120',
-                         'no overtaking',
-                         'no overtaking trucks',
-                         'priority at next intersection',
-                         'priority road',
-                         'give way',
-                         'danger',
-                         'construction')
+        # self._classes = ('__background__', # always index 0
+        #                  'Speed Limit 30',
+        #                  'Speed Limit 50',
+        #                  'Speed Limit 60',
+        #                  'Speed Limit 70',
+        #                  'Speed Limit 80',
+        #                  'Speed Limit 100',
+        #                  'Speed Limit 120',
+        #                  'no overtaking',
+        #                  'no overtaking trucks',
+        #                  'priority at next intersection',
+        #                  'priority road',
+        #                  'give way',
+        #                  'danger',
+        #                  'construction')
+        self._classes = ('__background__', 'no overtaking', 'Speed Limit 50', 'Speed Limit 100', 'Speed Limit 30', 'priority at next intersection', 'Speed Limit 60', 'danger', 'no overtaking trucks', 'construction', 'Speed Limit 80', 'Speed Limit 70', 'Speed Limit 120', 'give way', 'priority road')
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_ext = ['.jpg', '.png']
         self._image_index = self._load_image_set_index()
